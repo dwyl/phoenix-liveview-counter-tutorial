67a316b0b6130fafa753d49c97e4238707a3c4bc
nelsonic <nelson+github@dwyl.io>
2019-06-06 21:55:00 +0100

    update assertion in page_controller_test.exs to reflect counter on home page #3

--- test/live_view_counter_web/controllers/page_controller_test.exs
+++ test/live_view_counter_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LiveViewCounterWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "The count is"
   end
 end
