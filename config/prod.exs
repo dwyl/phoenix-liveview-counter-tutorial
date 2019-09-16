d5bfa2acdca07ffa67803ef1172fabf1890efb0c
nelsonic <nelson+github@dwyl.io>
2019-09-16 10:22:11 +0100

    System.get_env("PORT") #3

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_view_counter, LiveViewCounterWeb.Endpoint,
-  url: [host: "localhost", port: {:system, "PORT"}],
+  url: [host: "localhost", port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
