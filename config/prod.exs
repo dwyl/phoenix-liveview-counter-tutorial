6997d69843aaf740fc036b0c982de466239270c4
nelsonic <nelson+github@dwyl.io>
2019-09-16 10:33:43 +0100

    borrow config/prod.exs from https://github.com/dwyl/phoenix-chat-example/blob/master/config/prod.exs for #3

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_view_counter, LiveViewCounterWeb.Endpoint,
-  url: [host: "localhost", port: System.get_env("PORT")],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "phxchat.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
