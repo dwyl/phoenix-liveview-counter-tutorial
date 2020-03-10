06bca4152a32026a56ed9ee26a52fe17422a0d5b
nelsonic <nelson+github@dwyl.io>
2020-03-10 19:16:32 +0000

    snapshot of lib/live_view_counter_web/live/counter.ex at end of step 11

--- lib/live_view_counter_web/live/counter.ex
+++ lib/live_view_counter_web/live/counter.ex
@@ -1,31 +1,26 @@
 defmodule LiveViewCounterWeb.Counter do
   use Phoenix.LiveView
 
-  @topic "live"
-
-  def mount(_session, _params, socket) do
-    LiveViewCounterWeb.Endpoint.subscribe(@topic) # subscribe to the channel
+  def mount(_params, _session, socket) do
     {:ok, assign(socket, :val, 0),
       layout: {LiveViewCounterWeb.LayoutView, "app.html"}}
   end
 
-  def handle_event("inc", _value, socket) do
-    new_state = update(socket, :val, &(&1 + 1))
-    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
-    {:noreply, new_state}
+  def handle_event("inc", _, socket) do
+    {:noreply, update(socket, :val, &(&1 + 1))}
   end
 
   def handle_event("dec", _, socket) do
-    new_state = update(socket, :val, &(&1 - 1))
-    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
-    {:noreply, new_state}
-  end
-
-  def handle_info(msg, socket) do
-    {:noreply, assign(socket, val: msg.payload.val)}
+    {:noreply, update(socket, :val, &(&1 - 1))}
   end
 
   def render(assigns) do
-    LiveViewCounterWeb.PageView.render("counter.html", assigns)
+    ~L"""
+    <div>
+      <h1>The count is: <%= @val %></h1>
+      <button phx-click="dec">-</button>
+      <button phx-click="inc">+</button>
+    </div>
+    """
   end
 end
