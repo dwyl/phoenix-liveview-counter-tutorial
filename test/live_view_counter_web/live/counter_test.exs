92166f11a35485d01d8571a3aedff9d6c793f45c
nelsonic <nelson+github@dwyl.io>
2020-03-26 03:50:16 +0000

    incorporate @chrismccord feedback for *much* better test of handle_info https://elixirforum.com/t/how-to-test-handle-info-2-in-phoenix-liveview/30070/6

--- test/live_view_counter_web/live/counter_test.exs
+++ test/live_view_counter_web/live/counter_test.exs
@@ -1,30 +1,29 @@
 defmodule LiveViewCounterWeb.CounterTest do
   use LiveViewCounterWeb.ConnCase
   import Phoenix.LiveViewTest
-  # @endpoint MyEndpoint
 
   test "connected mount", %{conn: conn} do
     {:ok, _view, html} = live(conn, "/")
-    assert html =~ "The count is"
+    assert html =~ "Count: 0"
   end
 
   test "Increment", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
     # assert html_response(conn, 200) =~ "The count is"
-    assert render_click(view, :inc) =~ "The count is: 1"
+    assert render_click(view, :inc) =~ "Count: 1"
   end
 
   test "Decrement", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
     # assert html_response(conn, 200) =~ "The count is"
-    assert render_click(view, :dec) =~ "The count is: -1"
+    assert render_click(view, :dec) =~ "Count: -1"
   end
 
   test "handle_info/2", %{conn: conn} do
-    {:ok, view, _html} = live(conn, "/")
-    assert view.module == LiveViewCounterWeb.Counter
-    msg = %{payload: %{ val: 1 }}
-    result = send(view.pid, msg)
-    assert result == msg
+    {:ok, view, disconnected_html} = live(conn, "/")
+    assert disconnected_html =~ "Count: 0"
+    assert render(view) =~ "Count: 0"
+    send(view.pid, %{payload: %{ val: 1 }})
+    assert render(view) =~ "Count: 1"
   end
 end
