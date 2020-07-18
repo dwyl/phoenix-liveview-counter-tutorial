24576e9cae52cc7e9400bf78977958221eee36a8
nelsonic <nelson+github@dwyl.io>
2020-07-18 09:00:50 +0100

    update dependency on LiveView to 0.14.1 (latest released last week) 🚀

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LiveViewCounter.MixProject do
   def project do
     [
       app: :live_view_counter,
-      version: "0.13.2",
+      version: "0.14.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -37,16 +37,16 @@ defmodule LiveViewCounter.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix_live_view, "~> 0.14.1"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.2.7"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.2.2", override: true},
+      {:plug_cowboy, "~> 2.3", override: true},
 
       # Test Code Coverage:
       {:excoveralls, "~> 0.12.2", only: :test}
