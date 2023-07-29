0123455f65f633cb532c941d482984f3de968aa2
nelsonic <nelson+github@dwyl.com>
2023-07-29 06:22:43 +0100

    git rm -r lib test config assest mix.exs mix.lock #166

--- lib/live_view_counter/presence.ex
+++
@@ -1,5 +0,0 @@
-defmodule LiveViewCounter.Presence do
-  use Phoenix.Presence,
-  otp_app: :live_view_counter,
-  pubsub_server: LiveViewCounter.PubSub
-end
--- lib/live_view_counter_web/components/layouts.ex
+++
@@ -1,5 +0,0 @@
-defmodule LiveViewCounterWeb.Layouts do
-  use LiveViewCounterWeb, :html
-
-  embed_templates "layouts/*"
-end
